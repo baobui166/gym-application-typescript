7b95fdf2d952124fa85166d5626da414222be64c
Bui Chi Bao <buichibao1601@gmail.com>
2024-10-15 20:03:25 +0700

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import Navbar from "./scenes/navbar/Navbar";
 import { SelectedPage } from "./shared/types";
+import Navbar from "./scenes/navbar/Navbar";
+import Home from "./scenes/home/Home";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -30,6 +31,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
---
+++ src/scenes/home/Home.tsx
@@ -0,0 +1,101 @@
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import HomePageText from "../../assets/HomePageText.png";
+import HomePageGraphic from "../../assets/HomePageGraphic.png";
+import SponsorRedBull from "../../assets/SponsorRedBull.png";
+import SponsorForbes from "../../assets/SponsorForbes.png";
+import SponsorFortune from "../../assets/SponsorFortune.png";
+import { motion } from "framer-motion";
+
+interface Props {
+  setSelectedPage: (valye: SelectedPage) => void;
+}
+
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreen = useMediaQuery("(min-width:1060px)");
+
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* image and main header  */}
+      <motion.div
+        className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
+        {/* main header */}
+        <div className="z-10 mt-32 md:basic-3/5">
+          {/* headings */}
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="relative">
+              <div className="before:absolute before:-top-[2.7rem] before:-left-20 md:before:content-evolvetext before:z-[-1]">
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+            </div>
+
+            <p className="mt-8 text-sm md:text-start">
+              {" "}
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </motion.div>
+
+          {/* actions */}
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </motion.div>
+        </div>
+
+        {/* image */}
+        <div className="flex basic-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-center">
+          <img src={HomePageGraphic} alt="home-pageGraphic" />
+        </div>
+      </motion.div>
+
+      {/* sponsors */}
+      {isAboveMediumScreen && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img src={SponsorRedBull} alt="redbull-sponsor" />
+              <img src={SponsorForbes} alt="sponsor-forbes" />
+              <img src={SponsorFortune} alt="sponsor-fortune" />
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Home;
